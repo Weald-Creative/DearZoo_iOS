6e3333eda052bc24d6c781e7ed70706938b97020
Lee Probert <leeprobert@gmail.com>
2016-01-21 17:50:14 +0000

    Research into constraints. Not quite working.

--- DearZoo_iOS/DearZoo_iOS/GameViewController.swift
+++ DearZoo_iOS/DearZoo_iOS/GameViewController.swift
@@ -10,54 +10,60 @@ import UIKit
 import QuartzCore
 import SceneKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, SCNSceneRendererDelegate {
     
     @IBOutlet weak var scnView: SCNView!
     @IBOutlet weak var uiView: UIView!
     
+    var cameraNode: SCNNode?
+    var camera: SCNCamera?
+    var cameraOrbit: SCNNode?
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         // create a new scene
         let scene = SCNScene(named: "art.scnassets/DearZooScene.scn")!
-//        
-//        // create and add a camera to the scene
-//        let cameraNode = SCNNode()
-//        cameraNode.camera = SCNCamera()
-//        scene.rootNode.addChildNode(cameraNode)
-//        
-//        // place the camera
-//        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-//        
-        // create and add a light to the scene
-//        let lightNode = SCNNode()
-//        lightNode.light = SCNLight()
-//        lightNode.light!.type = SCNLightTypeOmni
-//        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-//        scene.rootNode.addChildNode(lightNode)
         
-        // create and add an ambient light to the scene
-//        let ambientLightNode = SCNNode()
-//        ambientLightNode.light = SCNLight()
-//        ambientLightNode.light!.type = SCNLightTypeAmbient
-//        ambientLightNode.light!.color = UIColor.darkGrayColor()
-//        scene.rootNode.addChildNode(ambientLightNode)
+        // init the camera
+        camera = SCNCamera()
+//        camera!.usesOrthographicProjection = true
+        camera!.zNear = 0
+        camera!.zFar = 1000
+        cameraNode = SCNNode()
+        cameraNode!.position = SCNVector3Make(0, 0, 50)
+        cameraNode!.camera = camera
+        cameraOrbit = SCNNode()
+        cameraOrbit!.addChildNode(cameraNode!)
+//        cameraOrbit?.rotation = SCNVector4Make(0, 0.5, 0, 0)
         
-        // retrieve the ship node
-//        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
-        // animate the 3d object
-//        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
+        scene.rootNode.addChildNode(cameraOrbit!)
+        
+//        let lookConstraint = SCNLookAtConstraint(target: scene.rootNode)
+//        let transConstraint = SCNTransformConstraint(inWorldSpace: true) { (node, m4) -> SCNMatrix4 in
+//        
+//            print(m4)
+//            let t = Float(self.scnView.sceneTime)
+//            let newMatrix4 = SCNMatrix4Translate(m4, cos(t)*6, 4+(cos(t)*2), sin(t)*10)
+//            return newMatrix4
+//        }
+        
+//        lookConstraint.gimbalLockEnabled = true
+//        camera!.constraints = [transConstraint]
+//        camera!.constraints = [lookConstraint, transConstraint]
         
         // set the scene to the view
         scnView!.scene = scene
         scene.background.contents = ["cubemap_side.png", "cubemap_side.png", "cubemap_top.png", "cubemap_bottom.png", "cubemap_side.png", "cubemap_side.png"]
         
         // allows the user to manipulate the camera
-        scnView!.allowsCameraControl = true
+//        scnView!.allowsCameraControl = true
+        cameraOrbit!.runAction(SCNAction.rotateByX(-0.5, y: 0, z: 0, duration: 0))
+        cameraOrbit!.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
         
         // show statistics such as fps and timing information
-//        scnView!.showsStatistics = true
-        
+        scnView!.showsStatistics = true
         
         // add a tap gesture recognizer
 //        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
@@ -65,6 +71,7 @@ class GameViewController: UIViewController {
     }
     
     func handleTap(gestureRecognize: UIGestureRecognizer) {
+        
         // retrieve the SCNView
         let scnView = self.view as! SCNView
         
@@ -72,6 +79,7 @@ class GameViewController: UIViewController {
         let p = gestureRecognize.locationInView(scnView)
         let hitResults = scnView.hitTest(p, options: nil)
         // check that we clicked on at least one object
+        
         if hitResults.count > 0 {
             // retrieved the first clicked object
             let result: AnyObject! = hitResults[0]
@@ -98,26 +106,13 @@ class GameViewController: UIViewController {
             SCNTransaction.commit()
         }
     }
+
+    // MARK: renderer delegate ---------------------------------------------------
     
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-    
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
-    
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    func renderer(renderer: SCNSceneRenderer, updateAtTime time: NSTimeInterval) {
+        
+//        let t = Float(time)
+//        cameraOrbit?.rotation = SCNVector4Make(cos(t)*6, 4+(cos(t)*2), 0, 0)
     }
 
 }
